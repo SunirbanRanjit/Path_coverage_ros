cb1175462647a0caabec1ca602ea7d881ae0736a
PSR0001 <83899035+PSR0001@users.noreply.github.com>
2021-12-16 21:22:44 +0530

    Create pathsequence.py

---
+++ multi_robot/darp/pathsequence.py
@@ -0,0 +1,260 @@
+from darp import DARP
+import numpy as np
+from kruskal import Kruskal
+from CalculateTrajectories import CalculateTrajectories
+from Visualization import visualize_paths
+import sys
+import random
+import argparse
+from turns import turns
+from pprint import pprint
+import io
+
+class DARPinPoly(DARP):
+    def clearPath(self):
+        arr=self.path[:]
+        max_len=0
+        for i in range(self.total):
+            max_len= max(max_len,len(arr[i]))
+        final=[]
+        for i in range(max_len):
+            f=[]
+            tup=[]
+            for j in range(self.total):
+                
+                temp=0
+                if i>=len(arr[j]):
+                    temp=arr[j][len(arr[j]) - 1][:2]
+                else:
+                    temp=arr[j][i][:2]
+                
+                tup.append(temp)
+            for k in range(self.total):
+                for l in range(2):
+                    f.append(tup[k][l])
+            final.append(f)
+        return final
+
+    def __init__(self, nx, ny, MaxIter, CCvariation, randomLevel, dcells, importance, notEqualPortions, initial_positions, portions, obstacles_positions, visualization,):
+        fck=0
+        self.total=len(initial_positions)
+        self.total=len(initial_positions)
+        DARP.__init__(self, nx, ny, MaxIter, CCvariation, randomLevel, dcells, importance, notEqualPortions, initial_positions, portions, obstacles_positions, visualization)
+        
+        if not self.success:
+            print("DARP did not manage to find a solution for the given configuration!")
+            sys.exit(0)
+        self.path=[]
+        mode_to_drone_turns = dict()
+        self.min_mode=0
+        min_avg=10000
+        for mode in range(4):
+            MSTs = self.calculateMSTs(self.BinaryRobotRegions, self.droneNo, self.rows, self.cols, mode)
+            AllRealPaths = []
+            for r in range(self.droneNo):
+                ct = CalculateTrajectories(self.rows, self.cols, MSTs[r])
+                ct.initializeGraph(self.CalcRealBinaryReg(self.BinaryRobotRegions[r], self.rows, self.cols), True)
+                ct.RemoveTheAppropriateEdges()
+                ct.CalculatePathsSequence(4 * self.init_robot_pos[r][0] * self.cols + 2 * self.init_robot_pos[r][1])
+                AllRealPaths.append(ct.PathSequence)
+                print(ct.PathSequence)
+                if fck<self.total:
+                    self.path.append(ct.PathSequence)
+                    fck+=1
+            
+                
+
+            TypesOfLines = np.zeros((self.rows*2, self.cols*2, 2))
+            
+            for r in range(self.droneNo):
+                flag = False
+
+                for connection in AllRealPaths[r]:
+                
+                    if flag:
+                        if TypesOfLines[connection[0]][connection[1]][0] == 0:
+                            indxadd1 = 0
+                        else:
+                            indxadd1 = 1
+
+                        if TypesOfLines[connection[2]][connection[3]][0] == 0 and flag:
+                            indxadd2 = 0
+                        else:
+                            indxadd2 = 1
+                    else:
+                        if not (TypesOfLines[connection[0]][connection[1]][0] == 0):
+                            indxadd1 = 0
+                        else:
+                            indxadd1 = 1
+                        if not (TypesOfLines[connection[2]][connection[3]][0] == 0 and flag):
+                            indxadd2 = 0
+                        else:
+                            indxadd2 = 1
+
+                    flag = True
+                    if connection[0] == connection[2]:
+                        if connection[1] > connection[3]:
+                            TypesOfLines[connection[0]][connection[1]][indxadd1] = 2
+                            TypesOfLines[connection[2]][connection[3]][indxadd2] = 3
+                        else:
+                            TypesOfLines[connection[0]][connection[1]][indxadd1] = 3
+                            TypesOfLines[connection[2]][connection[3]][indxadd2] = 2
+
+                    else:
+                        if (connection[0] > connection[2]):
+                            TypesOfLines[connection[0]][connection[1]][indxadd1] = 1
+                            TypesOfLines[connection[2]][connection[3]][indxadd2] = 4
+                        else:
+                            TypesOfLines[connection[0]][connection[1]][indxadd1] = 4
+                            TypesOfLines[connection[2]][connection[3]][indxadd2] = 1
+
+            subCellsAssignment = np.zeros((2*self.rows, 2*self.cols))
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    subCellsAssignment[2 * i][2 * j] = self.A[i][j]
+                    subCellsAssignment[2 * i + 1][2 * j] = self.A[i][j]
+                    subCellsAssignment[2 * i][2 * j + 1] = self.A[i][j]
+                    subCellsAssignment[2 * i + 1][2 * j + 1] = self.A[i][j]
+
+            drone_turns = turns(AllRealPaths)
+            drone_turns.count_turns()
+            mode_to_drone_turns[mode] = drone_turns
+            
+            if min_avg > drone_turns.ret_avg():
+                self.min_mode=mode
+
+            if self.visualization:
+                image = visualize_paths(AllRealPaths, subCellsAssignment, self.droneNo, self.color)
+                image.visualize_paths(mode)
+        
+        print("\nResults:\n")
+        min_mode=0
+        avg_turn=drone_turns.ret_avg()
+        print(avg_turn)
+        for mode, val in mode_to_drone_turns.items():
+            print(mode, val)
+            
+            
+        
+            
+
+    def CalcRealBinaryReg(self, BinaryRobotRegion, rows, cols):
+        temp = np.zeros((2*rows, 2*cols))
+        RealBinaryRobotRegion = np.zeros((2 * rows, 2 * cols), dtype=bool)
+        for i in range(2*rows):
+            for j in range(2*cols):
+                temp[i, j] = BinaryRobotRegion[(int(i / 2))][(int(j / 2))]
+                if temp[i, j] == 0:
+                    RealBinaryRobotRegion[i, j] = False
+                else:
+                    RealBinaryRobotRegion[i, j] = True
+
+        return RealBinaryRobotRegion
+
+    def calculateMSTs(self, BinaryRobotRegions, droneNo, rows, cols, mode):
+        MSTs = []
+        for r in range(self.droneNo):
+            k = Kruskal(rows, cols)
+            k.initializeGraph(self.BinaryRobotRegions[r, :, :], True, mode)
+            k.performKruskal()
+            MSTs.append(k.mst)
+        return MSTs
+
+if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser(
+        description=__doc__)
+    argparser.add_argument(
+        '-grid',
+        default=(10, 10),
+        type=int,
+        nargs=2,
+        help='Dimensions of the Grid (default: (10, 10))')
+    argparser.add_argument(
+        '-obs_pos',
+        default=[],
+        nargs='*',
+        type=int,
+        help='Dimensions of the Grid (default: (10, 10))')
+    argparser.add_argument(
+        '-in_pos',
+        default=[1, 3, 9],
+        nargs='*',
+        type=int,
+        help='Initial Positions of the robots (default: (1, 3, 9))')
+    argparser.add_argument(
+        '-nep',
+        action='store_true',
+        help='Not Equal Portions shared between the Robots in the Grid (default: False)')
+    argparser.add_argument(
+        '-portions',
+        default=[0.2, 0.3, 0.5],
+        nargs='*',
+        type=float,
+        help='Portion for each Robot in the Grid (default: (0.2, 0.7, 0.1))')
+    argparser.add_argument(
+        '-vis',
+        action='store_true',
+        help='Visualize results (default: False)')
+    args = argparser.parse_args()
+
+    rows, cols = args.grid
+
+    obstacles_positions = []
+    initial_positions = []
+
+    for position in args.in_pos:
+        if position < 0 or position >= rows*cols:
+            print("Initial positions should be inside the Grid.")
+            sys.exit(2)
+        initial_positions.append((position // cols, position % cols))
+
+    for obstacle in args.obs_pos:
+        if obstacle < 0 or obstacle >= rows*cols:
+            print("Obstacles should be inside the Grid.")
+            sys.exit(3)
+        obstacles_positions.append((obstacle // cols, obstacle % cols))
+
+    portions = []
+    if args.nep:
+        for portion in args.portions:
+            portions.append(portion)
+    else:
+        for drone in range(len(initial_positions)):
+            portions.append(1/len(initial_positions))
+
+    if len(initial_positions) != len(portions):
+        print("Portions should be defined for each drone")
+        sys.exit(4)
+
+    s = sum(portions)
+    if abs(s-1) >= 0.0001:
+        print("Sum of portions should be equal to 1.")
+        sys.exit(1)
+
+    for position in initial_positions:
+        for obstacle in obstacles_positions:
+            if position[0] == obstacle[0] and position[1] == obstacle[1]:
+                print("Initial positions should not be on obstacles")
+                sys.exit(3)
+
+    MaxIter = 80000
+    CCvariation = 0.01
+    randomLevel = 0.0001
+    dcells = 2
+    importance = False
+
+    print("\nInitial Conditions Defined:")
+    print("Grid Dimensions:", rows, cols)
+    print("Robot Number:", len(initial_positions))
+    print("Initial Robots' positions", initial_positions)
+    print("Portions for each Robot:", portions, "\n")
+    print(initial_positions)
+    poly = DARPinPoly(rows, cols, MaxIter, CCvariation, randomLevel, dcells, importance, args.nep, initial_positions, portions, obstacles_positions, args.vis)
+    a=poly.clearPath()
+    print(a) #ne baal ekhan theke ne.. 2 to kore pabi eta theke
+    #print(poly.path)  #ekhan theke 4 te kore pabi
+    no_bot=2
+    '''for i in range(len(a)):
+        (x1,y1),(x2,y2)=a[i]
+        print(x1,y1,x2,y2)'''
